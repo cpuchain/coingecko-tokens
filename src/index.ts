bff28869a89ad7a6e72733c133748114a112fe0c
CPUchain <info@cpuchain.org>
2025-05-08 02:40:46 +0000

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('Hello world!');
+import { writeFile } from 'fs/promises';
+
+async function test() {
+    await writeFile('./date.txt', Math.floor(Date.now() / 1000).toString());
+}
+
+test();
